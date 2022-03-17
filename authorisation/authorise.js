5f6aed80aa057f2acaa04b642cf23c75816b2751
DIVYA SHAKTI TRIPATHI <31913285+Divy001@users.noreply.github.com>
2022-03-18 00:25:48 +0530

    Update authorise.js

--- authorisation/authorise.js
+++ authorisation/authorise.js
@@ -4,10 +4,6 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 const authorisation = async (req, res, next) => {
     try {
         let userAuth = req.query.userId;
-        //let verifyId= ObjectId.isValid(userAuth);
-        // if(!verifyId) {
-        //     return res.status(400).send({ status:false, message: "Please Provide Valid Id. " })
-        // }
         if(!(req.userId == userAuth)){
             return res.status(400).send({ status:false, message: "You are not authorised to Perform this Task"});
         }
